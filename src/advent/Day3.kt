21dc3139ac6a0a3b87b0dd80b7be264e37fe550f
James Cooper <james.cooper@genedata.com>
2017-09-07 13:16:42 -0400

    added 3.2

--- src/advent/Day3.kt
+++ src/advent/Day3.kt
@@ -12,14 +12,48 @@ fun isTriangle(a: Int, b: Int, c: Int): Boolean {
 private fun input(prod: Boolean = false): List<String> {
 	return if (prod) readInput(3) else listOf(
 		" 5 10 25 ",
-		" 10 10 10 "
+		" 10 10 10 ",
+		" 12 10 10 "
 	)
 }
 
-fun main(args: Array<String>) {
-	val count = input(prod = true)
-		.filter(::isTriangle)
-		.count()
+fun <T> transposeBy3(list: List<T>): List<T> {
+	val out = ArrayList<T>()
+	for (i in 0 until list.size / 3) {
+		val col = i % 3
+		val row = i / 3
+		val index = row * 9 + col
+		out.add(list[index])
+		out.add(list[index + 3])
+		out.add(list[index + 6])
+	}
+	return out
+}
 
+fun main(args: Array<String>) {
+	val production = true
+	val transpose = true
+
+	var list = input(production)
+		.map { it.trim() }
+		.flatMap { it.split("\\s+".toRegex()) }
+		.map { it.toInt() }
+
+	if (transpose) {
+		list = transposeBy3(list)
+	}
+
+	var count = 0
+	for (i in 0 until list.size step 3) {
+		if (isTriangle(list[i], list[i + 1], list[i + 2])) {
+			count++
+		}
+	}
 	println(count)
+
+//	val count = input(prod = false)
+//		.filter(::isTriangle)
+//		.count()
+//
+//	println(count)
 }
