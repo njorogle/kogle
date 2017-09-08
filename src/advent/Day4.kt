3f8a687a6a82f9b44bd8dab2c01c3c88c844976c
James Cooper <james.cooper@genedata.com>
2017-09-08 12:36:11 -0400

    fix regex

--- src/advent/Day4.kt
+++ src/advent/Day4.kt
@@ -12,7 +12,7 @@ private fun input(prod: Boolean = false): List<String> {
 data class Code(val code: String, val sector: String, val checksum: String) {
 	companion object {
 		fun of(encoded: String): Code {
-			val regex = """([\w-]+)-(\d+)\[(\w{5})\]""".toRegex()
+			val regex = """([\w-]+)-(\d+)\[(\w{5})]""".toRegex()
 			val match: MatchResult = regex.matchEntire(encoded)!!
 			fun MatchResult.g(group: Int) = this.groupValues[group]
 			return Code(match.g(1), match.g(2), match.g(3))
